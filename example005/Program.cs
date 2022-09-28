8b884520353931ce3ff31f5717df8860e337fb4e
Ivan <grabovivan@gmail.com>
2022-09-28 20:02:07 +0300

    added example 5

--- example005/Program.cs
+++ example005/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "Маша"){
+    Console.WriteLine("Ура, это же Маша");
+}
+else{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
